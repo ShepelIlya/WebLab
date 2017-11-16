cc40722898e25ed38960ec41543e2b51e2a257c8
ShepelIlya <chepsiilya@gmail.com>
2017-11-16 20:50:14 +0300

    Added brush and eraser functions.

--- client/client.js
+++ client/client.js
@@ -1,51 +1,135 @@
+// io = require('socket.io').io;
 document.addEventListener("DOMContentLoaded", function() {
-   var mouse = { 
+      var mouse = { 
       click: false,
       move: false,
       pos: {x:0, y:0},
       pos_prev: false
-   };
-   // get canvas element and create context
-   var canvas  = document.getElementById('drawing');
-   var context = canvas.getContext('2d');
-   var width   = window.innerWidth;
-   var height  = window.innerHeight;
-   var socket  = io.connect();
+      };
+      // get canvas element and create context
+      var canvasDrawing  = document.getElementById('drawingLayer');
+      var canvasEvent  = document.getElementById('eventLayer');
+      
+      // var 
+      var context = canvasDrawing.getContext('2d');
+      var imageData;
 
-   // set canvas to full browser width/height
-   canvas.width = width;
-   canvas.height = height;
 
-   // register mouse event handlers
-   canvas.onmousedown = function(e){ mouse.click = true; };
-   canvas.onmouseup = function(e){ mouse.click = false; };
+      var width   = parseInt(canvasDrawing.getAttribute('width'));
+      var height  = parseInt(canvasDrawing.getAttribute('height'));
+      console.log('width=', typeof(width));
+      console.log('width=', width);   
+      console.log('height=', height);
+      var currentTool = 'brush';
+      var currentColor;
+      var currentSize = 10;
+      var socket  = io.connect();
 
-   canvas.onmousemove = function(e) {
-      // normalize mouse position to range 0.0 - 1.0
-      mouse.pos.x = e.clientX / width;
-      mouse.pos.y = e.clientY / height;
-      mouse.move = true;
-   };
-
-   // draw line received from server
-	socket.on('draw_line', function (data) {
-      var line = data.line;
-      context.beginPath();
-      context.moveTo(line[0].x * width, line[0].y * height);
-      context.lineTo(line[1].x * width, line[1].y * height);
-      context.stroke();
-   });
-   
-   // main loop, running every 25ms
-   function mainLoop() {
-      // check if the user is drawing
-      if (mouse.click && mouse.move && mouse.pos_prev) {
-         // send line to to the server
-         socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev ] });
-         mouse.move = false;
+      socket.on('get_server_image_data', function(data) {
+            console.log('get_server_image_data, data=')
+            console.log(data);
+            imageData = data;
+            var image = document.createElement("img");
+            image.setAttribute("src", imageData);
+            var timeout = setTimeout(function() {
+                  context.drawImage(image,0,0);
+            }, 200);
+            console.log(image);
+      });
+      socket.emit('request', imageData);                              imageData = canvasDrawing.toDataURL('png', 1);
+      imageData = canvasDrawing.toDataURL('png', 1);      
+      // socket.emit('draw_line', {
+      //       line: [ mouse.pos, mouse.pos_prev ],
+      //       dataArray: imageData
+      // });
+      function figureChanged(event) {
+            currentTool = event.target.getAttribute('data');
+            console.log('currentTool', currentTool);
       }
-      mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
-      setTimeout(mainLoop, 25);
-   }
-   mainLoop();
+      document.getElementById('brush').addEventListener("click", figureChanged);
+      document.getElementById('line').addEventListener("click", figureChanged);
+      document.getElementById('rect').addEventListener("click", figureChanged);
+      document.getElementById('circle').addEventListener("click", figureChanged);
+      document.getElementById('eraser').addEventListener("click", figureChanged);
+      
+
+      canvasEvent.onmousedown = function(e){
+            mouse.click = true;
+      };
+      canvasEvent.onmouseup = function(e){
+            console.log('canvasEvent.onmouseup');
+            mouse.click = false;
+            imageData = canvasDrawing.toDataURL('png', 1);
+            console.log("imageData=");
+            console.log(imageData);
+            socket.emit('post_server_image_data', imageData);
+      };
+
+      canvasEvent.onmousemove = function(e) {
+      // normalize mouse position to range 0.0 - 1.0
+            mouse.pos.x = e.offsetX / width;
+            mouse.pos.y = e.offsetY / height;
+            mouse.move = true;
+      };
+
+      // draw line received from server
+      socket.on('draw_line', function (data) {
+            var line = data.line;
+            context.beginPath();
+            context.moveTo(line[0].x * width, line[0].y * height);
+            context.lineTo(line[1].x * width, line[1].y * height);
+            context.stroke();
+            // console.log(context.getImageData(0,0,width,height).data.length);
+            // console.log("type=", typeof(console.log(context.getImageData(0,0,width,height).data)));
+      });
+
+      socket.on('eraser', function (data) {
+            var line = data.line;
+            context.clearRect(line[0].x * width - currentSize/2, line[0].y * height - currentSize/2, currentSize, currentSize);
+      });
+      
+      // socket.on('clear_server_data', function () {});           
+
+      // main loop, running every 25ms
+      function mainLoop() {
+            switch (currentTool) {
+                  case 'brush': {
+                        if (mouse.click && mouse.move && mouse.pos_prev) {
+                              // send line to to the server
+                              imageData = canvasDrawing.toDataURL('png', 1);
+                              // console.log(imageData);
+                              socket.emit('draw_line', {
+                                    line: [ mouse.pos, mouse.pos_prev ],
+                                    dataArray: imageData
+                              });
+                              mouse.move = false;
+                        }
+                  }
+                  case 'line': {
+
+                  }
+                  case 'rect': {
+                        
+                  }
+                  case 'circle': {
+                        
+                  }
+                  case 'eraser': {
+                        if (mouse.click && mouse.move && mouse.pos_prev) {
+                              console.log('eraser on');
+                              imageData = canvasDrawing.toDataURL('png', 1);                              
+                              // send line to to the server
+                              socket.emit('eraser', {
+                                    line: [ mouse.pos, mouse.pos_prev ],
+                                    dataArray: imageData
+                              });
+                              mouse.move = false;
+                        }                        
+                  }
+            }
+            mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
+            socket.emit('clear_server_data');
+            setTimeout(mainLoop, 25);
+      }
+      mainLoop();
 });
--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 var express = require('express'), 
-    app = express(),
-    http = require('http'),
-    socketIo = require('socket.io');
+app = express(),
+http = require('http'),
+socketIo = require('socket.io');
+var fs = require('fs');
 
 // start webserver on port 8080
 var server =  http.createServer(app);
@@ -11,22 +12,52 @@ server.listen(8080);
 app.use(express.static('C:/WebLab/client'));
 console.log("Server running on 127.0.0.1:8080");
 
-// array of all lines drawn
-var line_history = [];
-
+var imageData = "";
 // event-handler for new incoming connections
 io.on('connection', function (socket) {
+    
+    // first send the canvas data to the new client
+    socket.on('request', function (data) {
+        console.log("request");
+        console.log(imageData);
+        io.emit('get_server_image_data', imageData);
+    });
 
-   // first send the history to the new client
-   for (var i in line_history) {
-      socket.emit('draw_line', { line: line_history[i] } );
-   }
+    socket.on('post_server_image_data', function (data) {
+        console.log('post_server_image_data');
+        if (!data) {
+            console.log('imageData is empty');
+            return;
+        }
+        imageData = data;
+        var img = imageData.replace(/^data:image\/\w+;base64,/, "");
+        var buf = new Buffer(img, 'base64');
+        fs.writeFile('fileName.png', buf);
+        io.emit('get_server_image_data', imageData);
+    });
 
-   // add handler for message type "draw_line".
-   socket.on('draw_line', function (data) {
-      // add received line to history 
-      line_history.push(data.line);
-      // send line to all clients
-      io.emit('draw_line', { line: data.line });
-   });
+    // add handler for message type "draw_line".
+    socket.on('draw_line', function (data) {
+        // вот тут короче в файл записываем
+        imageData = data.dataArray;
+        var img = imageData.replace(/^data:image\/\w+;base64,/, "");
+        var buf = new Buffer(img, 'base64');
+        fs.writeFile('fileName.png', buf);
+        // send line to all clients
+        io.emit('draw_line', { line: data.line, });
+    });
+
+    socket.on('eraser', function (data) {
+        imageData = data.dataArray;
+        var img = imageData.replace(/^data:image\/\w+;base64,/, "");
+        var buf = new Buffer(img, 'base64');
+        fs.writeFile('fileName.png', buf);
+        // send line to all clients
+        io.emit('eraser', { line: data.line });
+    });
+
+    // socket.on('clear_server_data', function(){
+    //     imageData = '';
+    // });
+    
 });
